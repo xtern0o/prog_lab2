f3a805c74538383d1fda1b6945bffa1a9c2cf835
xtern0o <dos30028@gmail.com>
2024-10-17 13:33:20 +0300

    [fix] added Tsareena to battle

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,12 +8,14 @@ public class Main {
         Battle b = new Battle();
         Pokemon p1 = new Emolga("max", 50);
         Pokemon p2 = new Mantine("karn", 50);
-        Pokemon p3 = new Mantyke("maven", 40);
-        Pokemon p4 = new Steene("org.apache.maven.plugins", 40);
-        Pokemon p5 = new Bounsweet("java", 30);
+        Pokemon p3 = new Mantyke("maven", 50);
+        Pokemon p4 = new Steene("org.apache.maven.plugins", 50);
+        Pokemon p5 = new Bounsweet("java", 50);
+        Pokemon p6 = new Tsareena("honor magicbook", 50);
 
-        b.addAlly(p1);
+        b.addAlly(p6);
         b.addAlly(p2);
+        b.addAlly(p1);
         b.addFoe(p3);
         b.addFoe(p4);
         b.addFoe(p5);
